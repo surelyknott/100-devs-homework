4fdede2549f6f9c6f87ee743269ea5ec48074f3f
Jack Knott <jackhknott@hotmail.co.uk>
2026-01-08 18:17:41 +0000

    dnd subclass homework

--- class28-materials/dnd/js/main.js
+++ class28-materials/dnd/js/main.js
@@ -1,17 +1,42 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
 document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+function getFetch() {
+  const ul = document.querySelector('ul')
+  const choice = document.querySelector('input').value.trim().toLowerCase()
+  const slug = choice.replace(/\s+/g, '-')
+
+  ul.innerHTML = ''
+
+  if (!choice) {
+    ul.textContent = 'Enter a spell name.'
+    return
+  }
+
+  const url = `https://www.dnd5eapi.co/api/spells/${slug}`
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Request failed: ${res.status}`)
+        }
+        return res.json()
+      }) // parse response as JSON
       .then(data => {
-       
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+        const subclasses = data.subclasses || []
+        if (!subclasses.length) {
+          ul.textContent = 'No subclasses found for that spell.'
+          return
+        }
 
+        subclasses.forEach(obj => {
+          const li = document.createElement('li')
+          li.textContent = obj.name
+          ul.appendChild(li)
+        })
+    })
+    .catch(err => {
+          console.log(`error ${err}`)
+          ul.textContent = 'Spell not found. Check the spelling and try again.'
+    })
+}
