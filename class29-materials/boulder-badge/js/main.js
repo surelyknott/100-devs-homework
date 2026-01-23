7eed51c6973c1b7ebfbeab05a18b8c809a3dd96e
Jack Knott <jackhknott@hotmail.co.uk>
2026-01-23 16:48:10 +0000

    class 29 materials

---
+++ class29-materials/boulder-badge/js/main.js
@@ -0,0 +1,22 @@
+// Variables
+//You are getting ready to face Brock for the Boulder Badge. You want three pokemon on your team that each have evolved at least once. 
+//You have bulbasaur which is at level 5 and evolves at level 16, caterpie which is at level 1 and evolves at level 7, and weedle which is at level 1 and evolves at level 7. 
+let bulbaLevel = 5
+let caterpieLevel = 1
+let weedLevel = 1
+
+//Create three variables to store the number of rare candies each of your pokemon would need to evolve (rare candies increase your level by one). 
+let bulbCandy = 11
+let caterCandy = 6
+let weedCandy = 6
+
+let totalCandy = bulbCandy + caterCandy + weedCandy
+console.log(totalCandy)
+
+//Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
+
+//Conditionals And Functions
+//You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+
+//Loops
+//You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
