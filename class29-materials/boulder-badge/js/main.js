a99a24dfeab619e8e0eb1c94e46913ebea971d2c
Jack Knott <jackhknott@hotmail.co.uk>
2026-01-23 17:30:15 +0000

    pokemon js function challenges

--- class29-materials/boulder-badge/js/main.js
+++ class29-materials/boulder-badge/js/main.js
@@ -10,13 +10,40 @@ let bulbCandy = 11
 let caterCandy = 6
 let weedCandy = 6
 
+//Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
 let totalCandy = bulbCandy + caterCandy + weedCandy
 console.log(totalCandy)
 
-//Finally, create a fourth variable named totalCandies that sums all the rare candies you would need. 
-
 //Conditionals And Functions
-//You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+//You have a charmander in your party. 
+//Charmander can only battle if the temperature is above 0 degrees celcius. 
+//Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
+function tempConversion(temperature){
+    let convertedTemp = (temperature - 32) / 1.8
+    return convertedTemp
+}
+
+function canCharmanderBattle(currentTemp){
+    let converted = tempConversion(currentTemp)
+
+    if(converted > 0){
+    console.log('Charmander can battle!');
+    }
+    else{
+        console.log('Charmander cannot battle.')
+    }
+}
+
+canCharmanderBattle(33)
 
 //Loops
 //You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
+function pokeLeague (partySize){
+    for(let i = 1; i <= partySize; i++){
+        console.log('Pikachu, I choose you!')
+    }
+}
+pokeLeague(8)
+
+
+
