8eb5e29be241b7d7b25f2c913d0d64ea67624aaa
Jack Knott <jackhknott@hotmail.co.uk>
2026-02-11 01:20:32 +0000

    OOP practice

--- class31-materials/objects-child-class/js/main.js
+++ class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,47 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
+class Animal{
+    constructor(name){
+        this.name = name
+    }
+    speak(){
+        console.log(`${this.name} makes a sound!`)
+    }
+}
+class Dog extends Animal{
+    constructor(name, breed){
+        super(name)
+        this.breed = breed
+    }
+}
+class Chicken extends Animal{
+    constructor(name, gender){
+        super(name)
+        this.gender = gender
+    }
+}
+const clucker = new Chicken('Barry', 'male');
 
+
+class ArsenalPlayer{
+    constructor(name, age, position){
+        this.name = name
+        this.age = age
+        this.position = position
+    }
+    goal(){
+        console.log(`${this.name} scores a screamer!`)
+    }
+}
+class ArsenalNewSigning extends ArsenalPlayer{
+    constructor(name, age, position, formerClub, value, contractLength){
+        super(name, age, position)
+        this.formerClub = formerClub
+        this.value = value
+        this.contractLength = contractLength
+    }
+    announcement(){
+        console.log(`${this.name} signs on a ${this.contractLength} contract from ${this.formerClub} for ${this.value} million pounds!`)
+    }
+}
+const jackaldinho = new ArsenalNewSigning('Jackaldinho', 35, 'Right Wing', 'Napoli', 80, '3-year')
+const saka = new ArsenalPlayer('Saka', 24, 'Right Wing')
