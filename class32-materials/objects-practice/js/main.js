700f607497ca628bfdb48965b8d52334bfc64ecf
Jack Knott <jackhknott@hotmail.co.uk>
2026-02-18 23:29:25 +0000

    OOP practice

--- class32-materials/objects-practice/js/main.js
+++ class32-materials/objects-practice/js/main.js
@@ -1,8 +1,9 @@
 // Come up with with a parent class
 // Extend that parent class into two children
 // Use Encapsulation, Abstraction, Inheritance, and Polymorphism 
+
 class Contractor{
-    constructor(name,role){
+    constructor(name, role){
         this._name = name
         this._role = role
     }
@@ -13,39 +14,40 @@ class Contractor{
         return this._role
     }
     sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs!`)
+        console.log(`Hello, I'm on the ${this._role} team at #100Devs!`)
     }
 }
 class Front extends Contractor{
-    constructor(name,role,tech){
-        super(name,role)
+    constructor(name, role, tech){
+        super(name, role)
         this._tech = tech
     }
     get tech(){
         return this._tech
     }
     sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+        console.log(`Hello. I'm on the ${this._role} team at #100Devs and I use ${this._tech}.`)
     }
 }
 class Back extends Contractor{
-    constructor(name,role,tech){
-        super(name,role)
+    constructor(name, role, tech){
+        super(name, role)
         this._tech = tech
     }
     get tech(){
         return this._tech
     }
     sayHello(){
-        console.log(`Hello, I am on the ${this._role} team at #100Devs and I use ${this._tech}`)
+        console.log(`Hello. I'm on the ${this._role} team at #100Devs and I use ${this._tech}.`)
     }
 }
-let bob = new Contractor('Bob','Front-end')
-let simba = new Front('Simba','Front-end','React')
-let machi = new Back('The Machine','Back-end','Node')
 
-let agency = [bob,simba,machi]
+let bob = new Contractor ('Bob', 'Front-end')
+let jack = new Front ('Jack', 'Front-end', 'React')
+let jermaine = new Back ('Jermaine', 'Back-end', 'Node')
 
-for(person of agency){
+let agency = [bob, jack, jermaine]
+
+for (person of agency){ //doesn't have to be 'person'... can be anything
     person.sayHello()
 }
