b8ed2b2ae01e5fafa359ac93a1ddedec7396d330
Jack Knott <jackhknott@hotmail.co.uk>
2026-02-25 14:30:50 +0000

    more async practice with comments and additions

--- class35-materials/async-practice/js/main.js
+++ class35-materials/async-practice/js/main.js
@@ -27,20 +27,20 @@
 // houseThree()
 
 //Code 03
-function houseOne(){
-    console.log('Paper delivered to house 1')
-}
-function houseTwo(callback){
-    setTimeout(() => { // setTimeout becomes async and is handed over to the browser API or Node.js to handle, which in this case sets a timeout of 3 seconds...
-        console.log('Paper delivered to house 2') // before executing this console.log
-        callback() // immediately followed by this callback which is the houseThree console.log
-    }, 3000)
-}
-function houseThree(){
-    console.log('Paper delivered to house 3')
-}
-houseOne()
-houseTwo(houseThree) // houseTwo accepts a callback function as an argument, which is a higher order function 
+// function houseOne(){
+//     console.log('Paper delivered to house 1')
+// }
+// function houseTwo(callback){
+//     setTimeout(() => { // setTimeout becomes async and is handed over to the browser API or Node.js to handle, which in this case sets a timeout of 3 seconds...
+//         console.log('Paper delivered to house 2') // before executing this console.log
+//         callback() // immediately followed by this callback which is the houseThree console.log
+//     }, 3000)
+// }
+// function houseThree(){
+//     console.log('Paper delivered to house 3')
+// }
+// houseOne()
+// houseTwo(houseThree) // houseTwo accepts a callback function as an argument, which is a higher order function 
 
 //Code 04
 // function houseOne(){
@@ -54,23 +54,23 @@ houseTwo(houseThree) // houseTwo accepts a callback function as an argument, whi
 //         }, 4000)
 //     }, 5000)
 // }
-// houseOne()
+// houseOne() // you can nest all these setTimeout functions but it's messy
 
 //Code 05
-// const promise = new Promise((resolve, reject) => {
+// const promise = new Promise((resolve, reject) => { // a Promise object has an executor function that receives two functions when the promise is created (resolve and reject)
 //     const error = false
-//     if(!error){
-//         resolve('Promise has been fullfilled')
+//     if(!error){ // in other words, if true
+//         resolve('Promise has been fulfilled') // do this
 //     }else{
 //         reject('Error: Operation has failed')
 //     }
 // })
 // console.log(promise)
 // promise
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err))
+//     .then(data => console.log(data)) // these are called promise handlers, and are microtasks which run after sync lines finish. they wait for the promise to resolve and then execute the callback.
+//     .catch(err => console.log(err)) // same for this, except it executes the error callback, which in this case console logs 'Error: Operation has failed'.
 
-//Code 06
+// //Code 06
 // function houseOne(){
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
@@ -93,10 +93,10 @@ houseTwo(houseThree) // houseTwo accepts a callback function as an argument, whi
 //     })
 // }
 // houseOne()
+//     .then(data => console.log(data)) // needed if you want to show the console log but not needed for the chain which relies on returned promises instead 
+//     .then(houseTwo) // returns a promise 
 //     .then(data => console.log(data))
-//     .then(houseTwo)
-//     .then(data => console.log(data))
-//     .then(houseThree)
+//     .then(houseThree) // returns a promise
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
 
@@ -104,41 +104,130 @@ houseTwo(houseThree) // houseTwo accepts a callback function as an argument, whi
 // function houseOne(){
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             resolve('Paper delivered to house 1')
+//             const error = false
+//             if(!error){ // if error is false, then !error is true
+//                 resolve('Promise fulfilled: Paper delivered to house 1') // do this
+//             }else{
+//                 reject('Error: Operation has failed')
+//             }
 //         }, 1000)
 //     })
 // }
 // function houseTwo(){
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             resolve('Paper delivered to house 2')
+//             const error = false
+//             if(!error){ // if error is false, then !error is true
+//                 resolve('Promise fulfilled: Paper delivered to house 2') // do this
+//             }else{
+//                 reject('Error: Operation has failed')
+//             }
 //         }, 5000)
 //     })
 // }
 // function houseThree(){
 //     return new Promise((resolve, reject) => {
 //         setTimeout(() => {
-//             resolve('Paper delivered to house 3')
+//             const error = false
+//             if(!error){ // if error is false, then !error is true
+//                 resolve('Promise fulfilled: Paper delivered to house 3') // do this
+//             }else{
+//                 reject('Error: Operation has failed')
+//             }
 //         }, 2000)
 //     })
 // }
 
-// async function getPaid(){
-//     const houseOneWait = await houseOne()
-//     const houseTwoWait = await houseTwo()
-//     const houseThreeWait = await houseThree()
-//     console.log(houseOneWait)
-//     console.log(houseTwoWait)
-//     console.log(houseThreeWait)
+// async function getPaid(){ 
+//     try {
+//         const houseOneWait = await houseOne() // houseOne() starts its 1s timer and returns a pending Promise, await pauses inside getPaid until that promise resolves,
+//         console.log(houseOneWait) // after ~1s, houseOneWait becomes: "Promise fulfilled: Paper delivered to house 1"
+
+//         const houseTwoWait = await houseTwo() // houseTwo() doesn’t start until houseOne() resolves (because you call it after the first await)
+//         console.log(houseTwoWait)
+
+//         const houseThreeWait = await houseThree()
+//         console.log(houseThreeWait)
+        
+//     } catch (err) { // try/catch will catch any rejection from any awaited promise
+//         console.log(err)
+//     }
 // }
 
-// getPaid()
+// getPaid() // it’s an async function, so it returns a Promise immediately when called (even though you don’t use it)
 
 //Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    console.log(data)
+}
+getACuteDogPhoto()
 
+//Code 09, a version where Code 08 can has house functions that can all reject individually and decide whether to move on
+
+function houseOne(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const error = Math.random() < 0.4
+      if(!error){
+        resolve('House 1 success')
+      } else {
+        reject('House 1 failed')
+      }
+    }, 1000)
+  })
+}
+
+function houseTwo(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const error = Math.random() < 0.4
+      if(!error){
+        resolve('House 2 success')
+      } else {
+        reject('House 2 failed')
+      }
+    }, 1000)
+  })
+}
+
+function houseThree(){
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const error = Math.random() < 0.4
+      if(!error){
+        resolve('House 3 success')
+      } else {
+        reject('House 3 failed')
+      }
+    }, 2000)
+  })
+}
+
+async function getPaid(){ 
+  try {
+    const result1 = await houseOne()
+    console.log(result1)
+  } catch (err) {
+    console.log("Error at house 1:", err)
+  }
+
+  try {
+    const result2 = await houseTwo()
+    console.log(result2)
+  } catch (err) {
+    console.log("Error at house 2:", err)
+  }
+
+  try {
+    const result3 = await houseThree()
+    console.log(result3)
+  } catch (err) {
+    console.log("Error at house 3:", err)
+  }
+
+  console.log("Finished attempting all houses")
+}
+
+getPaid()
