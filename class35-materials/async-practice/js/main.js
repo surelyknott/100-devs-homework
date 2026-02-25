12f27e0863813ab070d331ecd6283c74c1a417e1
Jack Knott <jackhknott@hotmail.co.uk>
2026-02-25 12:33:23 +0000

    async practice

--- class35-materials/async-practice/js/main.js
+++ class35-materials/async-practice/js/main.js
@@ -27,20 +27,20 @@
 // houseThree()
 
 //Code 03
-// function houseOne(){
-//     console.log('Paper delivered to house 1')
-// }
-// function houseTwo(callback){
-//     setTimeout(() => {
-//         console.log('Paper delivered to house 2')
-//         callback()
-//     }, 3000)
-// }
-// function houseThree(){
-//     console.log('Paper delivered to house 3')
-// }
-// houseOne()
-// houseTwo(houseThree)
+function houseOne(){
+    console.log('Paper delivered to house 1')
+}
+function houseTwo(callback){
+    setTimeout(() => { // setTimeout becomes async and is handed over to the browser API or Node.js to handle, which in this case sets a timeout of 3 seconds...
+        console.log('Paper delivered to house 2') // before executing this console.log
+        callback() // immediately followed by this callback which is the houseThree console.log
+    }, 3000)
+}
+function houseThree(){
+    console.log('Paper delivered to house 3')
+}
+houseOne()
+houseTwo(houseThree) // houseTwo accepts a callback function as an argument, which is a higher order function 
 
 //Code 04
 // function houseOne(){
