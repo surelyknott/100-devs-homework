36b473f0998f51e5f9ef0fe0a851d895df5d1151
Jack Knott <jackhknott@hotmail.co.uk>
2026-03-01 11:53:30 +0000

    node.js practice

--- class35-materials/node-intro-demo/demo.js
+++ class35-materials/node-intro-demo/demo.js
@@ -2,8 +2,14 @@ const http = require('http')
 const fs = require('fs')
 http.createServer((req, res) => {
   fs.readFile('demofile.html', (err, data) => {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/plain'})
+      res.end('File not found')
+      return
+    }
     res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write(data)
-    res.end()
+    res.end(data)
   })
-}).listen(8000)
+}).listen(8000, () => {
+  console.log('Server running on http://localhost:8000')
+})
